1fa2c67aa978d2915cd5432b921fb4995cf07c64
CypCar <102803159+CypCar@users.noreply.github.com>
2023-12-07 09:54:48 +0100

    commit

---
+++ 10naBi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Podaj liczbe naturalna : ";
+	cin >> n;
+	int tab [1000];
+	int i=0;
+	while (n>0)
+	{
+		int reszta = n % 2;
+		n = n / 2;
+		tab[i++]= reszta;
+	}
+	for (int j=i-1; j>=0; j--)
+	cout << tab[j];
+	cout << endl;
+	getchar();
+	return 0;
+}
